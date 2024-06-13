604a2d55f3328860937a3d296691c76af4f4ae07
Andres-AM <andres.acunamarroquin@gmail.com>
2024-06-13 23:50:29 +0200

    script update

--- script2.R
+++ script2.R
@@ -10,18 +10,31 @@ code <- code |>
   filter(country_code == "CH")
 
 dr <- weather_forecast(location = code$name,
-                       hourly = c("temperature_2m", "precipitation",
+                       hourly = c("temperature_2m", 
+                                  "precipitation",
                                   "relative_humidity_2m",
                                   "wind_speed_10m","wind_gusts_10m",
                                   "cloud_cover"
                                   ),
                        start = today(),end = today())
 
+df <- dr|> 
+  mutate( date = date(datetime)) |> 
+  group_by(date) |> 
+  summarise( temp_min = min(hourly_temperature_2m), 
+             temp_max = max(hourly_temperature_2m),
+             wind_min = min(hourly_wind_speed_10m),
+             wind_max = max(hourly_wind_speed_10m),
+             hourly_precipitation = min(hourly_precipitation)
+  ) 
 
-dr
+df
 
 dr_daily <- weather_forecast("Geneva",
-                       daily = c("uv_index_clear_sky_max","uv_index_max"),
-                       start = today(),end = today(),)
+                       daily = c("uv_index_max"),
+                       start = today(),end = today())  
 
-dr_daily
+
+df <- df |> 
+  left_join(dr_daily,by = "date")
+df
