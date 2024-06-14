26af8765ee13b62f1c065e33cdd5fdfdaa448e80
Andres-AM <andres.acunamarroquin@gmail.com>
2024-06-14 17:16:17 +0200

    update

---
+++ libraries.R
@@ -0,0 +1,16 @@
+
+## All packages 
+library(tidyverse)
+library(lubridate)
+library(modelr)
+library(plotly)
+library(DT)
+library(shinydashboard)
+
+
+## Other settings 
+theme_set(theme_bw())
+options(digits=4,dplyr.summarise.inform=F,"lubridate.week.start" = 1)
+rm(list = ls())
+cat("\014")
+ 
--- script2.R
+++ script2.R
@@ -1,8 +1,6 @@
 
-
+source("libraries.R")
 library(openmeteo)
-library(tidyverse)
-library(lubridate)
 
 code <- geocode("geneva",n_results = 10,language = "en",silent = T)
 
@@ -25,16 +23,30 @@ df <- dr|>
              temp_max = max(hourly_temperature_2m),
              wind_min = min(hourly_wind_speed_10m),
              wind_max = max(hourly_wind_speed_10m),
+             wind_gusts_10m = max(hourly_wind_gusts_10m),
              hourly_precipitation = min(hourly_precipitation)
   ) 
-
-df
 
 dr_daily <- weather_forecast("Geneva",
                        daily = c("uv_index_max"),
                        start = today(),end = today())  
 
-
 df <- df |> 
   left_join(dr_daily,by = "date")
-df
+
+df |> 
+  ggplot()+
+  geom_errorbar(aes(xmin = temp_min,xmax = temp_max,y = wind_max),width = 2) +
+  geom_text(aes(x = temp_min, y = wind_max, label = temp_min), vjust = -4, color = "blue") +
+  geom_text(aes(x = temp_max, y = wind_max, label = temp_max), vjust = -4, color = "red") +
+  scale_x_continuous(n.breaks = 10,limits = c(-5,35)) +
+  scale_y_continuous(n.breaks = 10,limits = c(0,40)) +
+  geom_hline(yintercept = 10, linetype = 2, col = "grey")+
+  geom_vline(xintercept = 0, linetype = 2, col = "grey") +
+  theme(axis.text.x = element_text(angle = 0)) +
+  annotate("rect", xmin = -5, xmax = 10, ymin = 5, ymax = 40, alpha = 0.2, fill = "blue")+
+  labs(y = "wind", x = "temp",title  = paste0("Target"))
+
+
+
+ 
